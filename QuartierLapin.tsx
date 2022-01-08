404af41995c5cb8879eb400373c4f95af19841a1
maptest1212 <97365965+maptest1212@users.noreply.github.com>
2022-01-08 23:17:19 +0100

    tetst

---
+++ QuartierLapin.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="QuartierLapin" tilewidth="32" tileheight="32" tilecount="1768" columns="52">
+ <image source="QuartierLapin.png" trans="ff00ff" width="1664" height="1088"/>
+</tileset>
